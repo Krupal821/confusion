836c5286006dcee8fd7472175370e2a8d2d3b5dd
Krupal Patel <krupal821@gmail.com>
2022-10-25 17:33:41 -0400

    completed Assignment 3

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,10 +1,97 @@
-import react from "react";
-
-import {Card, CardImg, CardBody,CardText,Media, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import react, { Component } from "react";
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import {Card, CardImg, CardBody,CardText,Media, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem, Label, Row, Col, Button, Modal,ModalHeader, ModalBody} from 'reactstrap';
 import {Link} from 'react-router-dom';
 
 
-    
+class CommentForm  extends Component{
+    constructor(props){
+        super(props);
+    this.state = {  
+            isCommentToggleOpen: false
+    };
+    this.toggleCommentModel = this.toggleCommentModel.bind(this);
+    this.handleSubmitComment = this.handleSubmitComment.bind(this);
+
+    }
+    handleSubmitComment(values){
+        console.log('Current state is ' + JSON.stringify(values));
+        alert('Current state is ' + JSON.stringify(values));
+    }
+toggleCommentModel(){   // when 29 line button is clicked this funcion will be called(toggled)    in modal
+    this.setState({
+        isCommentToggleOpen: !this.state.isCommentToggleOpen   // once the function is toggles the comment witll be toggled in Modal and Modalheader
+    });
+}
+
+    render(){
+        const required = (val) => val && val.length;
+        const maxLength = (len) => (val) => !(val) || (val.length <= len);
+        const minLength = (len) => (val) => val && (val.length >= len);
+        return(
+            // <LocalForm onSubmit={(value) => this.handleSubmiComment}>
+            <div className="submit-comment">
+                <Row className="form-group">
+                    <Col md={{size: 12}}>
+                        {/* below button will toggle(Initally it's close) the comment modal when Clicked ---- line 11 bydefault its false */}
+                    <Button outline onClick={this.toggleCommentModel}>  
+                        <span className="fa fa-pencil fa-lg mr-2"></span>
+                        Submit Comment
+                    </Button>
+                    </Col>
+                </Row>
+                <Modal isOpen={this.state.isCommentToggleOpen} toggle={this.toggleCommentModel}>
+                <ModalHeader toggle={this.toggleCommentModel}>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(values) => this.handleSubmitComment(values)}>
+                    <Row className="form-group">
+                                <Col md={{size: 12}}>
+                                    <Label htmlFor="rating">Rating</Label>
+                                </Col>
+                                <Col md ={{size: 12}} >
+                                <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                                </Col>
+                    </Row>
+
+                    <Row className="form-group">
+                        <Col md={{size:12}}>
+                            <Label htmlFor="name">
+                                Your Name
+                            </Label>
+                            <Control.text model='.name' placeholder="Your Name" id="name" name="name" className="form-control" validators={{required, maxLength: maxLength(15), minLength: minLength(3)}}/>
+                            <Errors className="text-danger" model='.name' show='touched' messages={{required : 'is Required', maxLength: 'Must be 15 characters or less', minLength: 'Must be 3 characters or more'}}>
+
+                            </Errors>
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                                
+                                <Col md ={12} >
+                                <Label htmlFor="comments">Comment</Label>
+                                <Control.textarea className="form-control" model=".comments"  id="comments" name="comments" rows="12" validators={{required, maxLength: maxLength(10), minLength: minLength(3)}} />
+                                <Errors className="text-danger" model=".comments" show='touched' messages={{required: 'is Required', maxLength: 'Must be 10 characters or less',  minLength: 'Must be 3 characters or more'}}></Errors>
+                                </Col>
+
+                            </Row>
+
+                        <Row className="form-group">
+                            <Col md={{size: 12}}>
+                            <Button type="submit" color="primary"> Submit </Button>
+                            </Col>
+                        </Row>
+                    </LocalForm>
+                </ModalBody>
+                </Modal>
+                </div>
+        )
+    }
+}
   function  RenderDish({dish}) {   {/* created fucntion component with known props dish*/}
         if(dish != null){
             return(
@@ -49,7 +136,9 @@ import {Link} from 'react-router-dom';
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     {selectedDishComment}
-               
+                    <div className="container">
+                        <CommentForm />
+                    </div>
                 </div>
                 
             )
@@ -81,6 +170,7 @@ import {Link} from 'react-router-dom';
                          <RenderDish dish={props.dish}/>
                     </div>   
                         <RenderComments comments={props.comments}/>
+                   
                  </div>
             </div>
         )
